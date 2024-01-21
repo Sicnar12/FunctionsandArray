2e951054eae798d5500981e46f3c6ab0b853beaf
unknown <adetutu.francisb@gmail.com>
2024-01-21 15:13:07 -0500

    array

---
+++ app.js
@@ -0,0 +1,20 @@
+function sum(thing1,thing2) {
+    return thing1 + thing2
+}
+console.log(sum(1,2));
+
+function diff(thing1,thing2) {
+    return thing1- thing2
+    
+}
+console.log(diff(4,3));
+
+function multiply(thing1, thing2) {
+    return thing1 * thing2
+}
+console.log(multiply(2,5));
+
+function divide(thing1, thing2) {
+    return thing1 / thing2
+}
+console.log(divide(10,2));
